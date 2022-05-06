936b3e872d981f7572501585a9dfddf37c246d2b
lordsayur <umar_317@hotmail.com>
2022-05-07 06:29:42 +0800

    🔧 sb: add path alias

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,14 +1,20 @@
+const path = require("path");
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ],
-  "framework": "@storybook/vue3",
-  "core": {
-    "builder": "@storybook/builder-vite"
-  }
-}
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  framework: "@storybook/vue3",
+  core: {
+    builder: "@storybook/builder-vite",
+  },
+  viteFinal: (config) => {
+    config.resolve = {
+      ...config.resolve,
+      alias: {
+        ...config.resolve.alias,
+        "@": path.resolve(__dirname, "..", "src"),
+      },
+    };
+    return config;
+  },
+};
