2093bbb1e6c8fd5f69b13943d06cf2e9f141d670
lordsayur <umar_317@hotmail.com>
2022-05-14 07:52:20 +0800

    📦 add storybook interaction addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,12 @@
 const path = require("path");
 
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  stories: ["../src/**/*.stories.ts"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
   framework: "@storybook/vue3",
   core: {
     builder: "@storybook/builder-vite",
